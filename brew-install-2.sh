fcb7733a02bb14a9f96199786464519bed994925
Matteo Bianchi <37507190+mbianchidev@users.noreply.github.com>
2023-12-10 15:41:19 +0100

    Create brew-install-2.sh

---
+++ brew-install-2.sh
@@ -0,0 +1,67 @@
+autoconf
+autojump
+awk
+awscli
+azure-cli
+brotli
+c-ares
+ca-certificates
+cffi
+docker
+docker-completion
+docker-compose
+docutils
+gettext
+gnu-sed
+go
+grep
+groonga
+helm
+htop
+icu4c
+jq
+k9s
+kubernetes-cli
+libidn2
+libnghttp2
+libunistring
+libuv
+lz4
+m4
+mariadb
+mecab
+mecab-ipadic
+mpdecimal
+msgpack
+ncurses
+node
+nvm
+oniguruma
+openssl@3
+opentofu
+pcre
+pcre2
+pkg-config
+pycparser
+pyenv
+python-setuptools
+python@3.11
+python@3.12
+readline
+six
+sqlite
+unzip
+wget
+xz
+yq
+zsh-autosuggestions
+zstd
+
+brew install --cask dbeaver-community
+brew install --cask visual-studio-code
+brew install --cask slack
+brew install --cask google-chrome
+brew install --cask spotify
+brew install --cask mattermost
+brew install --cask 1password
+
